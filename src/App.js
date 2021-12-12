bb71d3f0700bc12d4119586a7471adfc4e95a0c6
Magdi7021 <92311360+Magdi7021@users.noreply.github.com>
2021-12-12 21:46:46 +0300

    HW10

--- src/App.js
+++ src/App.js
@@ -5,19 +5,36 @@ import { useEffect, useState } from "react"
 import { Route, Routes } from "react-router-dom"
 import { toast } from "react-toastify"
 import Sidebar from "./components/Sidebar"
+import Cast from "./pages/Cast"
 import Films from "./pages/Films"
+import Genres from "./pages/Genres"
 import FilmsContext from "./utils/FilmsContext"
 
 function App() {
   const [films, setFilms] = useState([])
+  const [genres, setGenres] = useState([])
+  const [casts, setCasts] = useState([])
 
   const getFilms = async () => {
     const response = await axios.get("http://localhost:5000/api/films")
     setFilms(response.data)
   }
 
+  const getGenres = async () => {
+    const response = await axios.get("http://localhost:5000/api/genres")
+    console.log(response.data)
+    setGenres(response.data)
+  }
+
+  const getCast = async () => {
+    const response = await axios.get("http://localhost:5000/api/cast")
+    setCasts(response.data)
+  }
+
   useEffect(() => {
     getFilms()
+    getGenres()
+    getCast()
   }, [])
 
   const deleteFilm = async filmId => {
@@ -35,9 +52,44 @@ function App() {
     }
   }
 
+  const deleteGenre = async genreId => {
+    try {
+      await axios.delete(`http://localhost:5000/api/genres/${genreId}`, {
+        headers: {
+          Authorization: localStorage.tokenDashboardFilms,
+        },
+      })
+      console.log(genreId)
+      toast.success("genre deleted")
+      getGenres()
+    } catch (error) {
+      if (error.response) toast.error(error.response.data)
+      else console.log(error)
+    }
+  }
+
+  const deleteCast = async castId => {
+    try {
+      await axios.delete(`http://localhost:5000/api/cast/${castId}`, {
+        headers: {
+          Authorization: localStorage.tokenDashboardFilms,
+        },
+      })
+      console.log(castId)
+      toast.success("cast deleted")
+      getGenres()
+    } catch (error) {
+      if (error.response) toast.error(error.response.data)
+      else console.log(error)
+    }
+  }
   const store = {
     films,
     deleteFilm,
+    genres,
+    deleteGenre,
+    casts,
+    deleteCast,
   }
 
   return (
@@ -45,15 +97,15 @@ function App() {
       <CssBaseline />
       <Box sx={{ display: "flex" }}>
         <Sidebar />
-
         <Box component="main" sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}>
           <Routes>
             <Route path="/films" element={<Films />} />
+            <Route path="/genres" element={<Genres />} />
+            <Route path="/casts" element={<Cast />} />
           </Routes>
         </Box>
       </Box>
     </FilmsContext.Provider>
   )
 }
-
 export default App
---
+++ src/pages/Genres.js
@@ -0,0 +1,33 @@
+import { Button } from "react-bootstrap"
+
+import { useContext } from "react"
+
+import { Table } from "react-bootstrap"
+
+import FilmsContext from "../utils/FilmsContext"
+
+import GenresCell from "../components/GenresCell"
+
+function Genres() {
+  const { genres } = useContext(FilmsContext)
+
+  return (
+    <Table bordered hover style={{ tableLayout: "fixed" }}>
+      <thead>
+        <tr>
+          <th style={{ width: "9%" }}>#</th>
+
+          <th style={{ width: "18%" }}>Name</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {genres.map(genre => (
+          <GenresCell key={genre._id} genre={genre} />
+        ))}
+      </tbody>
+    </Table>
+  )
+}
+
+export default Genres
