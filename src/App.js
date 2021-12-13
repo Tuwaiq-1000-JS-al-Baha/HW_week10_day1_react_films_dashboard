acacfb997c28358cba876b1929b38292fcf8255f
Najmah24 <najmah9022@gmail.com>
2021-12-13 09:49:03 +0300

    don

--- src/App.js
+++ src/App.js
@@ -7,9 +7,13 @@ import { toast } from "react-toastify"
 import Sidebar from "./components/Sidebar"
 import Films from "./pages/Films"
 import FilmsContext from "./utils/FilmsContext"
+import Casts from "./pages/Casts"
+import Genres from "./pages/Genres"
 
 function App() {
   const [films, setFilms] = useState([])
+  const [casts, setcasts] = useState([])
+  const [genras, setGenras] = useState([])
 
   const getFilms = async () => {
     const response = await axios.get("http://localhost:5000/api/films")
@@ -18,6 +22,22 @@ function App() {
 
   useEffect(() => {
     getFilms()
+  }, [])
+
+  const getCasts = async () => {
+    const response = await axios.get("http://localhost:5000/api/casts")
+    setcasts(response.data)
+  }
+  useEffect(() => {
+    getCasts()
+  }, [])
+
+  const getGenras = async () => {
+    const response = await axios.get("http://localhost:5000/api/genres")
+    setGenras(response.data)
+  }
+  useEffect(() => {
+    getGenras()
   }, [])
 
   const deleteFilm = async filmId => {
@@ -34,10 +54,43 @@ function App() {
       else console.log(error)
     }
   }
+  const deleteCast = async castId => {
+    try {
+      await axios.delete(`http://localhost:5000/api/cast/${castId}`, {
+        headers: {
+          Authorization: localStorage.tokenDashboardFilms,
+        },
+      })
+      toast.success("cast deleted")
+      getCasts()
+    } catch (error) {
+      if (error.response) toast.error(error.response.data)
+      else console.log(error)
+    }
+  }
+
+  const deleteGenra = async genraId => {
+    try {
+      await axios.delete(`http://localhost:5000/api/genras/${genraId}`, {
+        headers: {
+          Authorization: localStorage.tokenDashboardFilms,
+        },
+      })
+      toast.success("genra deleted")
+      getGenras()
+    } catch (error) {
+      if (error.response) toast.error(error.response.data)
+      else console.log(error)
+    }
+  }
 
   const store = {
     films,
     deleteFilm,
+    casts,
+    deleteCast,
+    deleteGenra,
+    genras,
   }
 
   return (
@@ -49,6 +102,8 @@ function App() {
         <Box component="main" sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}>
           <Routes>
             <Route path="/films" element={<Films />} />
+            <Route path="/casts" element={<Casts />} />
+            <Route path="/genres" element={<Genres />} />
           </Routes>
         </Box>
       </Box>
---
+++ src/components/GenraCall.js
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import { Button } from "react-bootstrap"
+
+import GenraViewModel from "./GenraViewModel"
+import GenraDeleteModal from "./GenraDeleteModel"
+function GenraCall(props) {
+  const { genra } = props
+  const [viewShow, setViewShow] = useState(false)
+  const [deleteShow, setDeleteShow] = useState(false)
+
+  return (
+    <tr style={{ verticalAlign: "middle", tableLayout: "fixed", wordWrap: "break-word" }}>
+      <td style={{ textOverflow: "ellipsis", whiteSpace: "nowrap", overflow: "hidden" }}>{genra._id}</td>
+      <td>{genra.name}</td>
+      <td>
+        <Button variant="info" className="me-2" onClick={() => setViewShow(true)}>
+          View
+        </Button>
+        <Button variant="success" className="me-2">
+          Edit
+        </Button>
+        <Button variant="danger" onClick={() => setDeleteShow(true)}>
+          Delete
+        </Button>
+      </td>
+      <GenraViewModel show={viewShow} setShow={setViewShow} genra={genra} />
+      <GenraDeleteModal show={deleteShow} setShow={setDeleteShow} genraId={genra._id} />
+    </tr>
+  )
+}
+
+export default GenraCall
